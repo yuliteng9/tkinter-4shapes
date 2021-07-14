069b37c53687efc415d4d64d62c21bbad0b43b85
yuliteng9 <li104.li@gmail.com>
2021-07-14 15:34:21 -0400

    Example commit

--- Covid-API/main.py
+++ Covid-API/main.py
@@ -1,1 +1,16 @@
-import random
+import requests
+import tkinter as tk
+
+response = requests.get("http://api.covid19api.com/summary")
+
+window = tk.Tk()
+window.geometry("800x400")
+window.title("Covid stats")
+
+for index in response.json()["Countries"]:
+    if index["Country"] == "Canada":
+        print(index)
+
+
+
+window.mainloop()
