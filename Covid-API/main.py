aeea8b1438d245cd1853dbe42aefab523401875e
yuliteng9 <li104.li@gmail.com>
2021-07-14 16:01:27 -0400

    Added button and entry to pick which country's confirmed cases.

--- Covid-API/main.py
+++ Covid-API/main.py
@@ -7,7 +7,8 @@ window = tk.Tk()
 window.geometry("800x400")
 window.title("Covid Stats")
 
-def show_data(country_name):
+def show_data():
+    country_name = country_entry.get()
     for index in response.json()["Countries"]:
         if index["Country"] == country_name:
             country_label = tk.Label(window, font = "Ariel 20", text = country_name)
@@ -16,6 +17,10 @@ def show_data(country_name):
             confirmed_label = tk.Label(window, text = "Total Confirmed Covid Cases: " + str(total_confirmed))
             confirmed_label.pack()
 
-show_data("Canada")
+
+country_entry = tk.Entry(window)
+country_entry.pack()
+search_button = tk.Button(window, text="Search", command=show_data)
+search_button.pack()
 
 window.mainloop()
