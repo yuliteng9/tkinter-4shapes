fb1ceb4d3ca0cfd37cbc6fd36c1938e00204dcfc
yuliteng9 <li104.li@gmail.com>
2021-07-14 15:54:28 -0400

    Show total confirmed cases Created function which can take a country name to show total confirmed for that country

--- Covid-API/main.py
+++ Covid-API/main.py
@@ -5,12 +5,17 @@ response = requests.get("http://api.covid19api.com/summary")
 
 window = tk.Tk()
 window.geometry("800x400")
-window.title("Covid stats")
+window.title("Covid Stats")
 
-for index in response.json()["Countries"]:
-    if index["Country"] == "Canada":
-        print(index)
+def show_data(country_name):
+    for index in response.json()["Countries"]:
+        if index["Country"] == country_name:
+            country_label = tk.Label(window, font = "Ariel 20", text = country_name)
+            country_label.pack()
+            total_confirmed = index["TotalConfirmed"]
+            confirmed_label = tk.Label(window, text = "Total Confirmed Covid Cases: " + str(total_confirmed))
+            confirmed_label.pack()
 
-
+show_data("Canada")
 
 window.mainloop()
